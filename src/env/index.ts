e55405139835132b6c9566cbf9d4d1cb3663cf2f
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-06-28 15:31:46 -0300

    fix: env types

--- src/env/index.ts
+++ src/env/index.ts
@@ -6,4 +6,12 @@ const environmentSchema = z.object({
   PORT: z.coerce.number().default(3333),
 })
 
-export const env = environmentSchema.parse(process.env)
+const _env = environmentSchema.safeParse(process.env)
+
+if (!_env.success) {
+  console.error('Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
